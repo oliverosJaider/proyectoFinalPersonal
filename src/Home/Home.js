9bdf488780d007ba6d3726e57fcf9595bc782a2b
407 <407@academicos.cesde.edu.co>
2022-11-29 16:42:57 -0500

    se crea proyecto

---
+++ src/Home/Home.js
@@ -0,0 +1,46 @@
+import './Home.css'
+export function Home(){
+
+    return(
+        <>
+            <section>
+                <div class="banner">
+
+                </div>
+            </section>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-8">
+                            <h2>Reik</h2>
+                            <p>Reik es una banda musical originaria de Mexicali, Baja California, México y formada en 2003. Esta banda está principalmente conformada por Jesús Navarro (Voz principal y Piano), Julio Ramírez (Guitarra y Bajo) y Gilberto «Bibi» Marín (Guitarra y Batería). Los primeros 5 álbumes de la banda mexicana han sido de puras Baladas románticas, sin embargo, desde principios de 2015 sus últimas producciones abarcan el reguetón y el pop latino. Ha ganado un Latin Billboard Music Award, cuatro premios Los Premios MTV Latinoamérica y un Grammy latino.</p>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyjaider.appspot.com/o/HSIQPPQMOZAMBO5ORVNZL5ACPY.jpg?alt=media&token=62c6e61e-723b-4fde-9154-9efb65bb1f9a"></img>
+                        </div>
+                        <div class="col-12 col-md-4">
+                        <i class="bi bi-vinyl-fill fs-1"></i>
+                        <i class="bi bi-vinyl-fill fs-1"></i> 
+                        <i class="bi bi-vinyl-fill fs-1"></i> 
+                        <i class="bi bi-vinyl-fill fs-1"></i> 
+                        </div>
+                    </div> 
+                </div>
+            </section>
+            <section>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyjaider.appspot.com/o/Reik-Maluma.jpg?alt=media&token=b6b717bf-68e3-4d69-bffe-bbf1e5262a64" alt="" class="img-fluid w-100"/>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyjaider.appspot.com/o/posibilidad-reik-invitados-sorpresas-celeste.jpeg?alt=media&token=dd81d616-c1c0-41be-8e42-a9d4cfc76c51" alt="" class="img-fluid w-100"/>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyjaider.appspot.com/o/reik-en-pachuca.jpeg?alt=media&token=4ce97f51-536d-4595-91ba-a229e22248b0" alt="" class="img-fluid w-100"/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+
+}
